05ccb76d943ff2ac01ca06b5f825ea5643095212
Matt Smith <mattsmith@Matts-MacBook-Pro.local>
2023-11-01 13:28:45 -0400

    SQH Assets

---
+++ test3.js
@@ -0,0 +1,98 @@
+function CallbackFunction(data) {
+  // This is where you handle your response. 'data' is the JSON object received from the server.
+  console.log("Inside Callback"); // For example, log the received data to the console
+  console.log(data.data.length);
+  // You could also parse or use this data in other ways, e.g., appending it to your webpage.
+  // Assuming it's a list of colors in the palette, you might do something like this:
+
+  var myDiv = document.getElementById("paintchip");
+
+  var tLength = data.data.colors.length;
+
+  // creates a <table> element and a <tbody> element
+  const tbl = document.createElement("table");
+  const tblBody = document.createElement("tbody");
+  var x = 0; // Table Columns
+  var y = 0; // Table Rows
+  var colors = data.data.colors;
+
+
+
+
+  if (data.data.colors) {
+    console.log("colors:" + data.data.colors.length);
+    
+    // Do something with each color, like appending them to a part of your webpage
+
+    // creating all cells
+    while (x < tLength) {
+      // creates a table row
+      const row = document.createElement("tr");
+
+      y = 0;
+
+      while (y < 6) {
+
+          // Create a <td> element and a text node, make the text
+          // node the contents of the <td>, and put the <td> at
+          // the end of the table row
+          const cell = document.createElement("td");
+          var colorDiv = document.createElement('div');
+          colorDiv.innerHTML = "<p>" + colors[x].name + "</p><p>" + colors[x].number + ":" + colors[x].hex + "</p>"; // assuming there's a 'name' field
+          colorDiv.style.width = "200px";
+          colorDiv.style.padding = "0.5rem";
+          colorDiv.style.borderBottom = "thin solid lightgray";
+          colorDiv.style.backgroundColor = "#" + colors[x].hex // assuming there's a 'hexValue' field
+
+          y++;
+          x++;
+
+          cell.appendChild(colorDiv);
+          row.appendChild(cell);
+      }
+
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
+
+    }    
+
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    myDiv.appendChild(tbl);
+    // sets the border attribute of tbl to '2'
+    tbl.setAttribute("border", "2");
+
+  }
+}
+
+$.ajax({
+  url: "//stage-api.benjaminmoore.com/api/88238a6f1629408b2cff/color/GetPaletteByCode",
+  data: "code=AF&colorData=true&countryCode=en-us",
+  dataType: "jsonp",
+  //jsonpCallback: "CallbackFunction",
+  success: function(response) {
+      // Handle successful response here if not using the 'CallbackFunction'
+      console.log(response);
+      CallbackFunction(response);
+  },
+  error: function(xhr, status, error) {
+    // Log the status and error to the console
+    console.error("AJAX Error: " + status + "; Error thrown: " + error);
+
+    // Additionally, you might want to log the entire xhr object for debugging
+    console.error(xhr);
+
+    // If you want more detailed server response text, you can log it like this:
+    if (xhr.responseText) {
+      try {
+          // Try to parse the error response from the server and log it
+          var jsonResponse = JSON.parse(xhr.responseText);
+          console.error("Server response:", jsonResponse);
+      } catch (e) {
+          // If the error message isn't valid JSON, just log the raw response
+          console.error("Raw server response:", xhr.responseText);
+      }
+    }
+  }
+});
---
+++ test4.js
@@ -0,0 +1,100 @@
+// Fixed CSV data as a string with line breaks
+const csvData = `
+MPI,NAME,NUMBER
+43,AURA Interior Paint- Satin,N526
+44,AURA Interior Paint- Eggshell,N524
+53,AURA Interior Paint- Matte,N522
+54,AURA Interior Paint- Semi-Gloss,N528
+1(V200-78Aluminumonly),Alkyd Urethane Enamel - Gloss,V200
+10,Cryli Cote® 100% Acrylic Exterior Paint - Semi-Gloss,N2
+10,Regal Select Exterior High Build, Flat,400
+10,Ultra Spec EXT Flat,447
+10,AURA Exterior Paint Flat,N629
+10,MoorLife Flat Finish,105
+104,TuffCrete® Solvent Acrylic Concrete Waterproofing Stain,CST-5XXX
+105,Aliphatic Acrylic Urethane - Gloss,V500
+105,Waterborne Urethane,V540
+107,Ultra Spec HP Acrylic Metal Primer,HP04
+11,Cryli Cote® 100% Acrylic Exterior Paint - Flat,N10
+11,Ultra Spec EXT Gloss,449
+11,Regal Select Exterior High Build, Soft Gloss,403
+114,MULTAPPLY™ Waterborne Acrylic Enamel - Gloss,1180
+114,Ultra Spec HP D.T.M. Acrylic Gloss,HP28
+114,Quick Dry Acrylic Spray DTM,V300
+114,MULTAPPLY™ Waterborne Acrylic Enamel - Gloss,1180
+116,Epoxy Mastic Coating,V160
+117,Texcrete® Silicone Water Repellent,194
+118,Super Kote 5000® Dry Fall Coating Latex - Flat,N110
+118,Benjamin Moore Latex Dry Fall - Flat,395
+121,Acrylic Polyurethane - Low Lustre,423
+129,Acrylic Polyurethane - Low Lustre,423
+`;
+
+function parseCSV(csvText) {
+    const rows = csvText.trim().split('\n');
+    const headers = rows[0].split(',');
+    const data = [];
+    const fSelect = document.getElementById("prod");
+
+//	console.log(fSelect.name);
+
+    for (let i = 1; i < rows.length; i++) {
+        const values = rows[i].split(',');
+        const rowData = {};
+
+        headers.forEach((header, index) => {
+            rowData[header] = values[index];
+        });
+	
+	//Populate form select with product number as options
+	var select = document.createElement("P");
+//	select.value = values[2];
+	select.innerHTML = values[2];
+
+	//fSelect.appendChild(select);
+	console.log(fSelect);
+
+        data.push(rowData);
+    }
+
+    return data;
+}
+
+// Function to make an AJAX request for an object
+function fetchDataForObject(obj) {
+    // Replace with your actual AJAX endpoint and data retrieval logic
+    const workingUrl = "https://stage-api.benjaminmoore.com/api/88238a6f1629408b2cff/product/GetProductDetail";
+    $.ajax({
+	    url: workingUrl,
+	    data: "productNumber=" + obj.NUMBER + "&countryCode=en-us",
+	    dataType: "jsonp",
+	    jsonpCallback: "CallbackFunction",
+	    error: function (xhr) {
+		// Handle errors here
+		    console.error('Failed to fetch data for ' + obj.NUMBER + ': Error was;' + xhr.status )
+	    },
+	    success: function (data) {
+		// Handle the successful response here
+		console.log(data);
+		console.log("Success: " + obj.NUMBER);
+	    }
+	});
+ 
+
+}
+
+function CallbackFunction(data) {
+	console.log("success");
+}
+
+const csvArray = parseCSV(csvData);
+
+// Iterate through the CSV array and make AJAX requests for each object
+csvArray.forEach(obj => {
+	//console.log(obj.NUMBER);
+	
+
+	window.setInterval(fetchDataForObject(obj), 80000);
+});
+
+
